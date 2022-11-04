ee29acc39ed2f495dc97ddabedb57e1651dfccb3
Asiedu Samuel <sasiedu@acsint.io>
2022-11-04 16:04:19 +0000

    chore(app): fix middleware no content type issue

--- helpers/Middlewares/ServiceFeatureMiddleware.cs
+++ helpers/Middlewares/ServiceFeatureMiddleware.cs
@@ -102,7 +102,7 @@ namespace helpers.Middlewares
 
 
                 var endpoint = new Endpoint(path);
-                if (context.Request.Method.ToLower() != "get" && context.Request.ContentType.Contains("multipart/form-data") &&  (context.Request?.Form?.Files?.Any()??false))
+                if (context.Request.Method.ToLower() != "get" && context.Request.ContentType !=null && context.Request.ContentType.Contains("multipart/form-data") &&  (context.Request?.Form?.Files?.Any()??false))
                 {
                     endpoint.Files = context.Request.Form.Files.Select((f) =>
                     {
